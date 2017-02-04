d6ad0b87ca4a55e9379cccac062ebdcdf203db99
yaxin.liu <yaxin.liu@ptthink.com>
2017-02-04 17:07:37 +0800

    代码优化

--- test.py
+++ test.py
@@ -2,6 +2,7 @@
 #-*- coding: UTF-8 -*-
 
 import sys
+import logging
 
 ########################################################################################################################
 ########################################################################################################################
@@ -11,12 +12,8 @@ sys.excepthook = myutil.util.my_excepthook
 
 if(__name__ == "__main__"):
     myutil.util.init_log_config()
-    print("running ...")
-    # import tcp_server
-    # tcp_server.start(8899)
 
-
-
-
-
+    from simple_tcp import tcp_server
+    tcp_server.start(8899)
+    logging.info("%s running over", __file__)
 
